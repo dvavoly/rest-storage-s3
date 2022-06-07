3b0b6e0c914eb5c833d4b5c72390f27ffda6bd88
Yevhen Danylov <dvavoly@outlook.com>
2022-06-07 15:57:23 +0300

    Initial commit

---
+++ src/main/java/org/example/storage/RestStorageS3Application.java
@@ -0,0 +1,13 @@
+package org.example.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestStorageS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestStorageS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/storage/RestStorageS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestStorageS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
